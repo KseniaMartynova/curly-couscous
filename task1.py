4cac288178eca4169519394329250c91f0c50af9
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2022-09-29 21:49:59 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import numpy
-import matplotlib.pyplot as mpp
+import math #импортируем код в модуль
+import numpy #поддержка больших массивов и матриц
+import matplotlib.pyplot as mpp # визуализация функции
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1)
+if __name__=='__main__': #указывает на область видимости, где будет выполняться код
+    arguments = numpy.arange(0, 200, 0.1) #задание промежутка
     mpp.plot(
         arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+        [math.sin(a) * math.sin(a/20.0) for a in arguments] #рисует график
     )
-    mpp.show()
+    mpp.show() #показывает график 
