aaa6bf14918dd203e952573c47e247374cc508b8
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2022-10-26 01:08:24 +0300

    Add files via upload

---
+++ cherepaxaFigna.py
@@ -0,0 +1,29 @@
+from turtle import*
+shape("turtle")
+speed(0)
+color('blue')
+pensize(1)
+
+def fu(size, levels, angle):
+    if levels==0:
+        
+        return
+    #forward(size)
+    right(angle)
+    forward(size)
+    fu(size*1.0,levels-1, angle)
+    left(angle*2)
+    fu(size*1.0,levels-1,angle)
+    right(angle)
+    left(angle*2)
+    fu(size*1.0,levels-1,angle)
+    right(angle)
+    backward(size)
+
+left(90)
+right(90)
+
+fu(50,5,60)
+
+mainloop()
+
