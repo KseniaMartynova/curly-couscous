41d190ded78846d88e6388818ffb3095460ebdfa
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2022-11-23 13:40:41 +0300

    Update task6-3.py

--- task6-3.py
+++ task6-3.py
@@ -2,12 +2,15 @@ def prost(n):
     for i in range(2,int(n**0.5)+1):
         if n%i==0:
             return False
+        
     return True
+
 a=int(input())
 if prost(a):
     print('True')
 else:
-    print('FAlse')
+    print('False')
+
 #проверка
 for n in range(2,a):
     if a%n==0:
