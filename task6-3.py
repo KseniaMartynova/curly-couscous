d5beb8a7e5869da6a6b6b13eaa14df662ff45d02
KseniaMartynova <113133074+KseniaMartynova@users.noreply.github.com>
2022-12-14 22:54:51 +0300

    Update task6-3.py

--- task6-3.py
+++ task6-3.py
@@ -1,24 +1,18 @@
-def prost(n):
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
+def prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
             return False
-        
     return True
 
-a=int(input())
-if prost(a):
+
+a = int(input())
+if prime(a):
     print('True')
 else:
     print('False')
 
-#проверка
-for n in range(2,a):
-    if a%n==0:
+# check
+for n in range(2, a):
+    if a % n == 0:
         print('False')
         break
-        
-    
-                
-
-        
-
